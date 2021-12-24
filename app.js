7dbf116c6066e597af9abc58e5b761c8c94524aa
bolattt <bolattny@gmail.com>
2021-12-24 01:53:08 -0500

    refactored with object

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
 const reset = document.querySelector("#reset");
 const gamesTotalEl = document.querySelector("select");
 const winnerDisplay = document.querySelector("#winnerDisplay");
 
-let p1Score = 0;
-let p2Score = 0;
 let gamesTotal = parseInt(gamesTotalEl.value);
 
 gamesTotalEl.addEventListener("change", (e) => {
@@ -15,41 +9,46 @@ gamesTotalEl.addEventListener("change", (e) => {
   resetFunc();
 });
 
-p1Button.addEventListener("click", () => {
-  p1Score++;
-  p1Display.textContent = p1Score;
-  if (p1Score === gamesTotal) {
-    winnerDisplay.textContent = "P1 Won!";
-    p1Button.disabled = true;
-    p2Button.disabled = true;
-    p1Display.style.color = "green";
-    p2Display.style.color = "red";
-  }
-});
-
-p2Button.addEventListener("click", () => {
-  p2Score++;
-  console.log(p2Score);
-  p2Display.textContent = p2Score;
-  if (p2Score === gamesTotal) {
-    winnerDisplay.textContent = "P2 Won!";
-    p1Button.disabled = true;
-    p2Button.disabled = true;
-    p2Display.style.color = "green";
-    p1Display.style.color = "red";
-  }
-});
-
 reset.addEventListener("click", resetFunc);
 
+const p1 = {
+  score: 0,
+  display: document.querySelector("#p1Display"),
+  button: document.querySelector("#p1Button"),
+};
+
+const p2 = {
+  score: 0,
+  display: document.querySelector("#p2Display"),
+  button: document.querySelector("#p2Button"),
+};
+
 function resetFunc() {
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = p1Score;
-  p2Display.textContent = p2Score;
+  for (let player of [p1, p2]) {
+    player.score = 0;
+    player.display.textContent = player.score;
+    player.button.disabled = false;
+    player.display.style.color = "black";
+  }
   winnerDisplay.innerHTML = "&nbsp";
-  p1Button.disabled = false;
-  p2Button.disabled = false;
-  p1Display.style.color = "black";
-  p2Display.style.color = "black";
+}
+
+p1.button.addEventListener("click", function () {
+  updateScore(p1, p2, "Player One");
+});
+
+p2.button.addEventListener("click", function () {
+  updateScore(p2, p1, "Player Two");
+});
+
+function updateScore(player, opponent, playerNumber) {
+  player.score++;
+  player.display.textContent = player.score;
+  if (player.score == gamesTotal) {
+    winnerDisplay.textContent = `${playerNumber} wins`;
+    player.button.disabled = true;
+    opponent.button.disabled = true;
+    player.display.style.color = "green";
+    opponent.display.style.color = "red";
+  }
 }
